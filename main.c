4f1f08dd372d4e34385f774b5d8f98de207eb885
ibacwc <kirzaka8@protonmail.com>
2024-12-07 22:33:55 +0200

    I spent hours on this wordwrap algorithm and ITS NOT EVEN PASSABLE. I am buying a rope. Programming is not for me.

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <string.h>
 #include <raylib.h>
-
 
 #define WIN_WIDTH 800
 #define WIN_HEIGHT 600
@@ -22,20 +23,46 @@ enum PAGE_TYPE { MILITARY,
 	TAX,
 	COMPLAINT,
 	SUGGESTION,
-	RANDOM
+	RANDOM,
+	INFO,
 };
 
 typedef struct Page_s{
 	char* text;
 	enum PAGE_TYPE type;
+	int clickedAt[2];
 	int position[2];
 	int following;
 }Page;
-Page ActivePage;
-Page tmp = {"bla bla blablah", TAX, {10, 50}, -1};
+
+Font terminus;
+Page* activePage;
+Page currPage;
+Page nextPage;
+Page tutorialPage;
 
 int SHOULD_RUN = 0;
-
+char* WrapText(char* text, int size, int lineSize){
+	int charsInLine = floor((float)lineSize/(size/2));
+	int textWidth = strlen(text)*size;
+	char* retVal = malloc((strlen(text) +textWidth/size+1)*sizeof(char));
+	retVal[0]='\0';
+	int lastIndex=0;
+	char* tmp = malloc(strlen(text) * sizeof(char));
+	tmp[0]='\0';
+	for(int i = 0; i<textWidth/size;++i){
+		if(i%charsInLine == 0 ){
+			if (i !=0) strcat(retVal, "\n");
+			strncpy(tmp, text+lastIndex, i-lastIndex);
+			tmp[i]='\0';
+			strcat(retVal, tmp);
+			lastIndex=i;
+		}
+	}
+	strcat(retVal, "\n");
+	strncat(retVal, text+lastIndex, strlen(text)-lastIndex);
+	return retVal;
+}
 int Collided(int x1, int y1, int w1, int h1,\
 			 int x2, int y2, int w2, int h2){
 	if (x1+w1 >= x2 &&\
@@ -44,11 +71,19 @@ int Collided(int x1, int y1, int w1, int h1,\
 		y1 <= y2+h2) return 1;
 	return 0;
 }
-
+void SetCurrentPage(Page* page){
+	page->following = -page->following;
+	if(page->following == 1)activePage = page;
+	else activePage = NULL;
+}
 int Init(){
 	InitWindow(WIN_WIDTH, WIN_HEIGHT, "game");
 	SetTargetFPS(60);
 	SetExitKey(KEY_DELETE);
+	terminus = LoadFontEx("data/terminus.ttf", 48, 0, 250);
+	char* tutorialText = "1 page = 0.08MJ. 1 folder = 100 pages = 8MJ. 1MJ = 0.0125$. A house needs 200MJ = 2.5$, you have 500 houses to heat (1250$). You can achieve this by paying (like you're supposed to) OR you can burn some documents to get that extra cash for yourself. Don't burn anything important, though, or you may lose your job and be arrested!";
+	char* brokenText = WrapText(tutorialText, 18, PAGE_WIDTH);
+	tutorialPage = (Page){brokenText, INFO, {0,0},{0,0}, -1};
 	return 2;
 }
 
@@ -62,26 +97,31 @@ void Input(){
 void Update(){
 	if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
 		if(Collided(GetMouseX(), GetMouseY(), 1, 1,\
-			tmp.position[0], tmp.position[1], PAGE_WIDTH, PAGE_HEIGHT)){
-			tmp.following = -tmp.following;
+			tutorialPage.position[0], tutorialPage.position[1], PAGE_WIDTH, PAGE_HEIGHT)){
+			tutorialPage.clickedAt[0] = GetMouseX()-tutorialPage.position[0];
+			tutorialPage.clickedAt[1] = GetMouseY()-tutorialPage.position[1];
+			SetCurrentPage(&tutorialPage);
 		}
 	}
-	if(tmp.following==1){
-		tmp.position[0]=GetMouseX()-H_PW;
-		tmp.position[1]=GetMouseY()-H_PH;
+	if(activePage){
+		activePage->position[0]=GetMouseX()-activePage->clickedAt[0];
+		activePage->position[1]=GetMouseY()-activePage->clickedAt[1];
 	}
 
+}
+void RenderPage(Page* page){
+	DrawRectangle(page->position[0], page->position[1], PAGE_WIDTH, PAGE_HEIGHT, BEIGE);
+	DrawTextEx(terminus, page->text, (Vector2){page->position[0]+5,page->position[1]+10},18, 0,BLACK);
 }
 void Render(){
 	BeginDrawing();
 	ClearBackground(RAYWHITE);
 	
-	DrawRectangle(tmp.position[0], tmp.position[1], PAGE_WIDTH, PAGE_HEIGHT, BEIGE);
-
+	RenderPage(&tutorialPage);
 	
 	if(SHOULD_RUN == 1){
 		DrawRectangle(0,200,800,200,BLACK);
-		DrawText("Exit? [y/N]", 280, 250, 48, WHITE);
+		DrawText("Exit? [y/N]", 280, 250, 32, WHITE);
 	}
 
 	EndDrawing();
